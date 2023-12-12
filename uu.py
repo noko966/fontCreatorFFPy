720ddd6aaf48b9b9fd98619f43fd66dd8aa6a72b
Nikolay Kananov <nikolay.kananov@Digitain.com>
2023-12-12 12:19:44 +0400

    finished

--- uu.py
+++ uu.py
@@ -12,7 +12,8 @@ font = fontforge.open(os.path.join(source_folder, source_blank_font))
 css_class_prefix = "es_glyph_"
 font_file_name = "eSportsIcons"
 font_family_name = "{}Font".format(font_file_name)
-
+css_class_additional = "es_glyph_large"
+ico_preview_size = "16"
 files = os.listdir(src_icons_path)
 
 filesArray = []
@@ -27,11 +28,21 @@ html_content = """
     <link rel="stylesheet" href="styles.css" />
     <style>
   .icons_container {
-        display: flex;
-      flex-direction: row;
-      gap: 8px;
-      flex-wrap: wrap;
-  }
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          gap: calc(var(--icoSize) * 0.3);
+          flex-wrap: wrap;
+          padding-left: calc(var(--icoSize) * 0.5);
+          padding-right: calc(var(--icoSize) * 0.5);
+          background: var(--bodyBg);
+          border: 1px solid var(--151A1E);
+      }
+
+  html {
+  background: var(--bodyBg);
+  color: var(--bodyTxt);
+}
   </style>
   </head>
   <body>
@@ -53,22 +64,42 @@ css_content = f"""
   font-display: swap;
 }}
 
-.ico_font-L {{
-  width: 36px !important;
-  height: 36px !important;
-  line-height: 36px !important;
-  font-size: 36px !important;
+:root{{
+  --bodyBg: #121212;
+  --bodyBg2: #151A1E;
+  --bodyTxt: #ccc;
+  --dominantBg: #0b293d;
+  --dominantBg2: #14496d;
+  --dominantTxt: #fefefe;
+  --accentBg: #00b6ff;
+  --accentBg2: #33454d;
+  --accentTxt: rgba(0, 0, 0, 0.97);
+  --icoSize: {ico_preview_size}px;
 }}
+
+.{css_class_additional} {{
+  width: var(--icoSize) !important;
+  height: var(--icoSize)  !important;
+  line-height: var(--icoSize)  !important;
+  font-size: calc(var(--icoSize)) !important;
+  background: var(--dominantBg);
+  color: var(--dominantTxt);
+  border-radius: 4px;
+  transition: all 0.3s;
+  border: 1px solid var(--dominantBg2);
+  cursor: pointer;
+}}
+
 
 [class^="{css_class_prefix}"],
 [class*=" {css_class_prefix}"] {{
   font-family: "{font_family_name}";
   display: inline-block;
   flex-shrink: 0;
-  width: 24px;
-  height: 24px;
-  line-height: 24px;
-  font-size: 24px;
+  width: var(--icoSize);
+  height: var(--icoSize);
+  line-height: var(--icoSize);
+  font-size: var(--icoSize);
   text-align: center;
   vertical-align: middle;
   font-weight: normal;
@@ -89,6 +120,20 @@ for file in files:
 
 print(len(filesArray))
 
+def process_string(s):
+    result = ""
+    for i, char in enumerate(s):
+        if char.isupper():
+            # If it's the start of the string or follows a space, convert to lowercase
+            if i == 0 or s[i-1] == ' ':
+                result += char.lower()
+            else:
+                # Otherwise, add an underscore before it and convert to lowercase
+                result += "_" + char.lower()
+        else:
+            result += char
+    return result
+
 for idx, files in enumerate(filesArray):
     uv = "uniE006"
     hexToInt = int("E000", 16) + idx
@@ -97,12 +142,18 @@ for idx, files in enumerate(filesArray):
     glyph = font.createMappedChar("uni" + intToHex)
     glyph.importOutlines(os.path.join(src_icons_path, files))
 
+
+    print(dir(glyph))
+    print(glyph.width)
+    print(glyph.vwidth)
+    print(glyph.boundingBox())
+
     result = files.replace(" ", "")
     result = result.replace(".svg", "")
-    result = result.replace("a__", "")
+    result = process_string(result)
 
     html_content += f"""
-    <i class="{css_class_prefix}{result}"></i>
+    <i class="{css_class_prefix}{result} {css_class_additional}"></i>
     """
 
     css_content += f"""
@@ -117,7 +168,7 @@ html_content += """
 </html>
     """
 
-# font.save("dist/output-font.sfd")
+font.save("dist/output-font.sfd")
 
 font.generate("dist/esportIcons.ttf")
 font.generate("dist/esportIcons.eot")
